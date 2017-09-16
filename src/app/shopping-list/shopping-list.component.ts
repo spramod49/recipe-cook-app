f77658a2d708f80236e6eaa1c46f78ff6c68acf3
Pramod Rao <spramod49@gmail.com>
2017-09-17 00:29:16 +0530

    Added shopping list, and test ingredients.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import  {Ingredient} from "../shared/ingredient.model";
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-    ingredients=[];
+    ingredients:Ingredient[] = [
+        new Ingredient("Potato" ,3),
+        new Ingredient("Tomatoes" ,10),
+        new Ingredient("Apples" ,10)
+
+];
   constructor() { }
 
   ngOnInit() {
